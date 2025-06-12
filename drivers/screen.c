9592b8a4560e8cf1053f9cee365f3aecd2583186
Rohan Lambture <rohanlambture13@gmail.com>
2025-06-12 16:55:33 +0530

    Fix screen drivers

--- drivers/screen.c
+++ drivers/screen.c
@@ -9,6 +9,7 @@ void set_cursor_offset(int offset);
 int get_offset(int col, int row);
 int get_offset_row(int offset);
 int get_offset_col(int offset);
+void error();
 
 /* Public functions */
 void printk_at(char *message,int col,int row , int color){
@@ -17,13 +18,18 @@ void printk_at(char *message,int col,int row , int color){
 		color = WHITE_ON_BLACK;
 	}
 	int index = 0;
-	int offset;
-	while(message[index] !='\0'){
-		offset = printk_char(message[index++],col,row,color);
-		row = get_offset_row(offset);
-		col = get_offset_col(offset);
+	int offset = get_offset(col,row);
+	while(message[index]!='\0'){
+		char *vga = (char*)VGA_MEMORY_ADDRESS;
+		vga[offset] = message[index++];
+		vga[offset+1] = color;
+		offset+=2;
 	}
-	// set_cursor_offset(get_offset(col, row));
+	// while(message[index] !='\0'){
+	// 	offset = printk_char(message[index++],col,row,color);
+	// 	row = get_offset_row(offset);
+	// 	col = get_offset_col(offset);
+	// }
 }
 
 void printk(char *message){
@@ -39,30 +45,32 @@ void clear_screen(int col,int row){
 		vga[offset+1]= WHITE_ON_BLACK;
 		offset+=2;
 	}
+	set_cursor_offset(get_offset(col, row));
 }
 
 /* Private functions implementation */
 
 int printk_char(char c,int col,int row,int color){
-	if(col >= SCREEN_WIDTH || row >= SCREEN_HEIGHT){
-		char *vga = (char*)VGA_MEMORY_ADDRESS;
-		vga[2*SCREEN_HEIGHT*SCREEN_WIDTH-2]= 'X';
-		vga[2*SCREEN_HEIGHT*SCREEN_WIDTH-1]= RED_ON_WHITE;
-	}
+	// if(col >= SCREEN_WIDTH || row >= SCREEN_HEIGHT){
+	// 	// error();
+	// }
 	int offset = get_offset(col,row);
-	if(!color){
-		color = WHITE_ON_BLACK;
-	}
-	if(offset < 0 ){
-		offset = get_cursor_offset();
-	}else if(offset >= 2 * SCREEN_HEIGHT * SCREEN_WIDTH){
-		// for(int i=1;i<SCREEN_HEIGHT;i++){
-		// 	memcpy((char*)VGA_MEMORY_ADDRESS+get_offset(0,i-1),(char*)VGA_MEMORY_ADDRESS+get_offset(0,i),SCREEN_WIDTH * 2);
-		// }
-		// Blank last line
-		// clear_screen(0, SCREEN_HEIGHT-1);
-	}
+	// if(!color){
+	// 	color = WHITE_ON_BLACK;
+	// }
+	// if(offset < 0 ){
+	// 	offset = get_cursor_offset();
+	// }
+	// else if(offset >= 2 * SCREEN_HEIGHT * SCREEN_WIDTH){
+	// 	// error();
+	// 	// for(int i=1;i<SCREEN_HEIGHT;i++){
+	// 	// 	memcpy((char*)VGA_MEMORY_ADDRESS+get_offset(0,i-1),(char*)VGA_MEMORY_ADDRESS+get_offset(0,i),SCREEN_WIDTH * 2);
+	// 	// }
+	// 	// Blank last line
+	// 	// clear_screen(0, SCREEN_HEIGHT-1);
+	// }
 	if(c == '\n'){
+		error();
 		row++;
 		col = 0;
 		offset = get_offset(col,row);
@@ -107,3 +115,11 @@ int get_offset_row(int offset){
 int get_offset_col(int offset){
 	return (offset % (SCREEN_WIDTH * 2))/2;
 }
+int error_pos = 2 * SCREEN_WIDTH * SCREEN_HEIGHT ;
+void error(){
+	char *vga = (char*)VGA_MEMORY_ADDRESS;
+	vga[error_pos-2]= 'X';
+	vga[error_pos-1]= RED_ON_WHITE;
+	// error_pos -=2;
+}
+
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,7 +2,7 @@
 #include "util.h"
 
 int main() {
-	printk("hello world\nNext line");
+	printk("hello world1234567890NextlineRohan Lambturedfkadfkdkfdakldjfkajfkdjfkdjfkdfkjadlfoerka;dficnal;nda");
 	// printk_at("hello",80,30, 0x0f);
 	// clear_screen(0,0);
 	return 0;
