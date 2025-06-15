1a845ad3bcc2f2ee9bf2ec4f3c8b11e4e1c1b054
Rohan Lambture <rohanlambture13@gmail.com>
2025-06-15 19:48:19 +0530

    Video Scroll

---
+++ kernel/util.c
@@ -0,0 +1,46 @@
+#include "util.h"
+
+void mempcpy(char *dest, const char *src, int num_bytes){
+	int i = 0;
+	while(i < num_bytes){
+		dest[i] = src[i];
+		i++;
+	}
+}
+
+void int_to_ascii(int num, char *str){
+	int i = 0;
+	int is_negative = 0;
+	
+	// Handle negative numbers
+	if(num < 0){
+		is_negative = 1;
+		num = -num;
+		str[i++] = '-';
+	}
+	
+	// Handle zero case
+	if(num == 0){
+		str[i++] = '0';
+		str[i] = '\0';
+		return;
+	}
+	
+	// Extract digits (they'll be in reverse order)
+	int start = i;
+	while(num > 0){
+		str[i++] = num % 10 + '0';
+		num /= 10;
+	}
+	str[i] = '\0';
+	
+	// Reverse the digits portion
+	int end = i - 1;
+	while(start < end){
+		char temp = str[start];
+		str[start] = str[end];
+		str[end] = temp;
+		start++;
+		end--;
+	}
+}
---
+++ kernel/util.h
@@ -0,0 +1,2 @@
+void mempcpy(char *dest,const char *src,int num_bytes);
+void int_to_ascii (int num, char *str);
