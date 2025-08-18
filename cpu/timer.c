9ca687ca48cf86fa0f0ba4d4763d1d7c75dd1707
Rohan Lambture <rohanlambture13@gmail.com>
2025-08-18 22:35:28 +0530

    TODO:Fix general protection fault

--- cpu/timer.c
+++ cpu/timer.c
@@ -20,20 +20,13 @@ void init_timer(uint32_t frequency){
 	// set keyboard handler in interrupt array
 	register_interrupt_handler(IRQ0, timer_callback);
 
-	// TODO: better way to handle this
-	if(frequency == 0) return;
-
 	// TODO: hardcoded value of PIT base frequency ? 
 	uint32_t divisor = 1193180 / frequency;
 	uint8_t low =  lower_8(divisor);
 	uint8_t high = higher_8(divisor);
 
-	asm volatile ("cli");
 
 	port_byte_out(0x43, 0x36);
 	port_byte_out(0x40, low);
 	port_byte_out(0x40, high);
-
-	kprint("Checkpoint3.1\n");
-	// asm volatile ("sti");
 }
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,23 +5,12 @@
 
 int main() {
 	clear_screen();
-	kprint("Checkpoint1\n");
 	
 	isr_install();
-	kprint("Checkpoint2\n");
 	asm volatile("sti");
 
-	kprint("Checkpoint3\n");
-	init_timer(5);
+	init_timer(50);
 
-	// TODO: Fix sti issue 
-	// asm volatile("sti");
-	kprint("Checkpoint4\n");
-
-	// for (int i = 0; i < 24; i++) {
-	// 	char str[255];
-	// 	int_to_ascii(i, str);
-	// 	kprint_at(str, 0, i);
-	// }
-
+	while(1){
+	}
 }
