ec26a1b679410337afb35f9bcd2c3e48c41928b0
Rohan Lambture <rohanlambture13@gmail.com>
2025-08-19 01:54:46 +0530

    Basic keyboard drivers

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,15 +1,13 @@
 #include "../drivers/screen.h"
 #include "../cpu/isr.h"
 #include "../cpu/timer.h"
+#include "../drivers/keyboard.h"
 
 int main() {
 	clear_screen();
 	
 	isr_install();
 	asm volatile("sti");
-	init_timer(50);
-
-	while(1){
-
-	}
+	// init_timer(50);
+	init_keyboard();
 }
