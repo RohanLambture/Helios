f9255dc9af157eb3e4b2b2bbbec8273a883c86b3
Rohan Lambture <rohanlambture13@gmail.com>
2025-08-29 03:29:45 +0530

    fixed:Backspace goes beyond prompt

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -29,8 +29,10 @@ static void keyboard_callback(struct registers_t regs) {
 	
 	if (scancode > SC_MAX) return;
 	if (scancode == BACKSPACE) {
-		backspace(key_buffer);
-		kprint_backspace();
+		if(strlen(key_buffer) > 0){
+			backspace(key_buffer);
+			kprint_backspace();
+		}
 	} else if (scancode == ENTER) {
 		kprint("\n");
 		user_input(key_buffer); /* kernel-controlled function */
